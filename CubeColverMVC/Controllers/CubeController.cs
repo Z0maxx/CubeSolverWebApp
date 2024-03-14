d03d51187c80a52d90d347ebe4c3d1db2453011d
nyari.zoltan@stud.uni-obuda.hu <nyari.zoltan@stud.uni-obuda.hu>
2024-03-14 08:49:45 +0100

    added color identifier

--- CubeColverMVC/Controllers/CubeController.cs
+++ CubeColverMVC/Controllers/CubeController.cs
@@ -12,7 +12,7 @@ namespace CubeColverMVC.Controllers
     public class CubeController : ControllerBase
     {
         [HttpPost]
-        public async Task<IEnumerable<int>> Solve(int[][][] cubeColors)
+        public IEnumerable<int> Solve(int[][][] cubeColors)
         {
             int[] a = cubeColors.SelectMany(x => x).SelectMany(x => x).Select(x => x).ToArray();
             string b = string.Join(' ', a);
@@ -31,6 +31,40 @@ namespace CubeColverMVC.Controllers
             string moves = solver.StandardOutput.ReadToEnd();
             return moves.Replace("\"", "").Split(' ')[0..^1].Select(x => int.Parse(x));
 
+        }
+
+        [HttpPost]
+        public CubeResultImage IdentifyColors(CubeImage image)
+        {
+            string file = $"{Guid.NewGuid()}.dat";
+            System.IO.File.WriteAllBytes(file, image.Pixels.Select(p => (byte)p).ToArray());
+            var identifier = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "CubeColorIdentifier.exe",
+                    CreateNoWindow = true,
+                    Arguments = $"{image.Width} {image.Height} {file}",
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                }
+            };
+            identifier.Start();
+            string error = identifier.StandardError.ReadToEnd();
+            CubeResultImage resultImage = new();
+            if (error == "")
+            {
+                string[] result = identifier.StandardOutput.ReadToEnd().Split('\n');
+                string[] widthHeight = result[0].Split(' ');
+                string[] resultColors = result[1].Split('|');
+                resultImage.Colors = resultColors;
+                resultImage.Pixels = System.IO.File.ReadAllBytes(file).Select(p => (int)p).ToArray();
+                resultImage.ResultWidth = int.Parse(widthHeight[0]);
+                resultImage.ResultHeight = int.Parse(widthHeight[1]);
+            }
+            System.IO.File.Delete(file);
+            return resultImage;
         }
     }
 }
---
+++ CubeColverMVC/Models/CubeResultImage.cs
@@ -0,0 +1,10 @@
+﻿namespace CubeColverMVC.Models
+{
+    public class CubeResultImage
+    {
+        public int[] Pixels { get; set; }
+        public string[] Colors { get; set; }
+        public int ResultWidth { get; set; }
+        public int ResultHeight { get; set; }
+    }
+}
