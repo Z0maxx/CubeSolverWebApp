a49497802868ba0d06da1df0b5f5b947a217ed66
Z0maxx <nyari.zoltan500@gmail.com>
2024-07-19 08:35:14 +0200

    added solve classes

---
+++ CubeColverMVC/Models/CubeSolve.cs
@@ -0,0 +1,12 @@
+﻿namespace CubeColverMVC.Models
+{
+    public class CubeSolve
+    {
+        public IEnumerable<int> Moves { get; set; }
+        public string? Error { get; set; }
+        public CubeSolve()
+        {
+            Moves = new List<int>();
+        }
+    }
+}
---
+++ CubeColverMVC/Models/SolveRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CubeColverMVC.Models
+{
+    public class SolveRequest
+    {
+        public int[][][] Colors { get; set; }
+        public bool WhiteCross { get; set; }
+        public SolveRequest()
+        {
+            Colors = Array.Empty<int[][]>();
+        }
+    }
+}
